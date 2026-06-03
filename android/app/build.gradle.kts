e0cec6a21478d534748d76fa26487667858c9d57
vyaesop <100709552+vyaesop@users.noreply.github.com>
2026-06-03 13:00:35 +0300

    Fix release build: explicitly disable isShrinkResources
    Resource shrinking requires minification to also be enabled.
    Since isMinifyEnabled = false, set isShrinkResources = false too.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
         release {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 
